e52bc4ca7bd7adad5673b6fe21bae915aa7a58e7
yzrilyzr <asdfg636254@163.com>
2017-09-05 13:00:11 +0800

    server 蛤

--- FAQ/src/main/java/com/yzrilyzr/FAQ/Data/Group.java
+++
@@ -1,9 +0,0 @@
-package com.yzrilyzr.FAQ.Data;
-
-public class Group extends ToStrObj
-{
-	public int faq;
-	public String nick,sign;
-	public int[] members=new int[0];
-	public int[] role=new int[0];
-}
